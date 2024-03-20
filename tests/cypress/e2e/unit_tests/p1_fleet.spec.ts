a7e26285f18c89f1d5dca8d1bdb3156ce38e46d4
Satyajit Bulage <satyajit.bulage@suse.com>
2024-03-20 21:44:06 +0530

    Add test for GitRepo name with K8s compliant name and trim bundle name for long GitRepo name (#69)
    * Add test for GitRepo name with K8s compliant name
    
    Fixes: #68
    
    Signed-off-by: Satyajit Bulage <satyajit.bulage@suse.com>
    
    * Added test to master-e2e
    
    Signed-off-by: Satyajit Bulage <satyajit.bulage@suse.com>
    
    * Updated with advanced get status of GitRepo
    
    Signed-off-by: Satyajit Bulage <satyajit.bulage@suse.com>
    
    * Renamed test file name
    
    Signed-off-by: Satyajit Bulage <satyajit.bulage@suse.com>
    
    * fixed extra navigation problem
    
    Signed-off-by: Satyajit Bulage <satyajit.bulage@suse.com>
    
    * Added 4 more test cases in single test file
    
    Signed-off-by: Satyajit Bulage <satyajit.bulage@suse.com>
    
    * Updated tests and it's name as per suggestion
    
    Signed-off-by: Satyajit Bulage <satyajit.bulage@suse.com>
    
    ---------
    
    Signed-off-by: Satyajit Bulage <satyajit.bulage@suse.com>

---
+++ tests/cypress/e2e/unit_tests/p1_fleet.spec.ts
@@ -0,0 +1,101 @@
+/*
+Copyright © 2023 - 2024 SUSE LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import '~/support/commands';
+import * as cypressLib from '@rancher-ecp-qa/cypress-library';
+import { qase } from 'cypress-qase-reporter/dist/mocha';
+
+beforeEach(() => {
+  cy.login();
+  cy.visit('/');
+  cy.deleteAllFleetRepos();
+});
+
+
+Cypress.config();
+describe('Test GitRepo Bundle name validation and max character trimming behavior in bundle', () => {
+  const branch = "master"
+  const path = "simple-chart"
+  const repoUrl = "https://github.com/rancher/fleet-test-data/"
+
+  const repoTestData: testData[] = [
+    { qase_id: 103,
+      repoName: "test-test-test-test-test-test-test-test-test-t",
+      test_explanation: "47 characters long is NOT TRIMMED but PATH is added with '-' to 53 characters" },
+    { qase_id: 104,
+      repoName: "test-test-test-test-test-test-test-test-test-test-test-test",
+      test_explanation: "59 characters long is TRIMMED to 53 characters max" },
+    { qase_id: 106,
+      repoName: "test-test-test-test-123-456-789-0--test-test-test-test",
+      test_explanation: "54 characters long is TRIMMED to 53 characters max" },
+    { qase_id: 105,
+      repoName: "Test.1-repo-local-cluster",
+      test_explanation: "INVALID and NORMAL characters" },
+    { qase_id: 61,
+      repoName: "ryhhskh-123456789+-+abdhg%^/",
+      test_explanation: "INVALID and SPECIAL characters" },
+  ]
+
+  repoTestData.forEach(
+    ({ qase_id, repoName, test_explanation }) => {
+
+      if ((qase_id === 105 || qase_id === 61)) {
+        qase(qase_id,
+          it(`Fleet-${qase_id}: Test GitRepo NAME with "${test_explanation}" displays ERROR message and does NOT get created`, () => {
+            // Add Fleet repository and create it
+            cy.addFleetGitRepo({repoName, repoUrl, branch, path});
+            cy.clickButton('Create');
+
+            // Assert errorMessage exists
+            cy.get('[data-testid="banner-content"] > span')
+              .should('contain', repoName)
+              .should('contain', 'RFC 1123')
+
+            // Navigate back to GitRepo page
+            cy.clickButton('Cancel')
+            cy.contains('No repositories have been added').should('be.visible')
+          })
+          )
+      } else {
+        qase(qase_id,
+          it(`Fleet-${qase_id}: Test GitRepo bundle name TRIMMING behavior. GitRepo with "${test_explanation}"`, () => {
+            // Change namespace to fleet-local
+            cy.fleetNamespaceToggle('fleet-local');
+
+            // Add Fleet repository and create it
+            cy.addFleetGitRepo({repoName, repoUrl, branch, path});
+            cy.clickButton('Create');
+            cy.verifyTableRow(0, 'Active', repoName);
+
+            // Navigate to Bundles
+            cypressLib.accesMenu("Advanced")
+            cypressLib.accesMenu("Bundles")
+
+            // Check bundle name trimed to less than 53 characters
+            cy.contains('tr.main-row[data-testid="sortable-table-1-row"]').should('not.be.empty', { timeout: 25000 });
+            cy.get(`table > tbody > tr.main-row[data-testid="sortable-table-1-row"]`)
+              .children({ timeout: 300000 })
+              .should('not.have.text', 'fleet-agent-local')
+              .should('not.be.empty')
+              .should('include.text', 'test-')
+              .should(($ele) => {
+                expect($ele).have.length.lessThan(53)
+              })
+            cy.deleteAllFleetRepos();
+          })
+        )
+      }
+    }
+  )
+});
